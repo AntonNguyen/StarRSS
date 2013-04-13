10749299410c661929eb3b421b7d249345a6868f
fotcorn <fotcorn@gmail.com>
2013-04-13 14:43:58 +0200

    bugfix: wait until input animation is done, then show scrolling text

--- javascripts/guts.js
+++ javascripts/guts.js
@@ -3,6 +3,49 @@
     $(function() {
         var animationEnd = "animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd";
         var transitionEnd = "webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd";
+        var fetch = null;
+        
+        $("input").keyup(function(event) {
+            if (event.keyCode === 13) {            	
+            	var rss = $(this).val();
+                fetch = $.ajax({
+                	url: 'http://ajax.googleapis.com/ajax/services/feed/load',
+                	data: {v: '1.0', q: rss, num: 30},
+                	dataType: 'jsonp',
+                });
+                document.getElementById("falcon_fly").play();
+                return $(this).parent().addClass("zoomed");
+            }
+        });
+        $(".input").on(transitionEnd, function() {
+        	return showResponse();
+        });
+        
+        showResponse = function() {
+            $(".plane").show();
+            return fetch.done(function(data) {
+                var entries = data.responseData.feed.entries;
+        		var messages = [];
+        		for (i = 0; i < entries.length; i++) {
+        			messages.push(entries[i].title)
+        		}
+        		$(".plane").show();
+        		playCommit(messages);
+            }).fail(function(problem) {
+                console.log(problem);
+                return playError();
+            });
+        };
+        
+        playCommit = function(messages) {
+            document.getElementById("theme").play();
+            return crawl(messages);
+        };
+        
+        playError = function() {
+            document.getElementById("imperial_march").play();
+            return crawl([ "Tun dun dun, da da dun, da da dun ...", "Couldn't find the repo, the repo!" ]);
+        };
         
         crawl = function(messages) {
             var counter, delay;
@@ -24,38 +67,11 @@
                 return counter = 0;
             }
         };
-        playCommit = function(messages) {
-            document.getElementById("theme").play();
-            return crawl(messages);
-        };
-        playError = function() {
-            document.getElementById("imperial_march").play();
-            return crawl([ "Tun dun dun, da da dun, da da dun ...", "Couldn't find the repo, the repo!" ]);
-        };
+        
         $(document).on(animationEnd, ".content", function() {
             return $(this).remove();
         });
-        $("input").keyup(function(event) {
-            if (event.keyCode === 13) {            	
-            	var rss = $(this).val();
-                $.ajax({
-                	url: 'http://ajax.googleapis.com/ajax/services/feed/load',
-                	data: {v: '1.0', q: rss, num: 30},
-                	dataType: 'jsonp',
-                	success: function(data) {
-                		var entries = data.responseData.feed.entries;
-                		var messages = [];
-                		for (i = 0; i < entries.length; i++) {
-                			messages.push(entries[i].title)
-                		}
-                		$(".plane").show();
-                		playCommit(messages);
-                	}
-                });
-                document.getElementById("falcon_fly").play();
-                return $(this).parent().addClass("zoomed");
-            }
-        });
+        
         return $(".input").show();
     });
 }).call(this);
